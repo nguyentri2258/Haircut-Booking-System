4388468e8b61f850b78aa2486e6f7f59477294b8
nguyentri2258 <nguyentri2258@gmail.com>
2026-02-04 13:28:31 +0700

    Update table

--- database/migrations/2025_04_23_103525_create_addresses_table.php
+++ database/migrations/2025_04_23_103525_create_addresses_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('address');
             $table->string('note')->nullable();
             $table->timestamps();
